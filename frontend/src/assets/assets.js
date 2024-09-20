aec550153b1c5c0023c33978391e0c20272e7521
unknown <Sunayana007>
2024-09-20 18:25:54 +0530

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,41 @@
+import leaf_wallpaper from './d_wp.jpg'
+import kids_1 from './fwp.jpg'
+import kids_2 from './kwp1.jpg'
+import flower_1 from './fwp.jpg'
+import flower_2 from './fwp1.jpg'
+import wallpaper from './rwp.jpg'
+import wallpaper_1 from './wp.jpg'
+import logo from './NaturalWallpaper.png'
+import search_icon from './search icon.jpg'
+import profile_icon from './profile icon.jpg'
+import cart_icon from './cart icon.jpg'
+
+
+export const assets = {
+    leaf_wallpaper,
+    kids_1,
+    kids_2,
+    flower_1,
+    flower_2,
+    wallpaper,
+    wallpaper_1,
+    logo,
+    search_icon,
+    profile_icon,
+    cart_icon
+    
+}
+
+export const products = [
+    {
+        _id:"aaaaa",
+        name:"wallpaper",
+        description:"abcd" ,
+        price:100,
+        image:[leaf_wallpaper],
+        category:"natural wallpaper",
+        size:"1mtr",
+        bestseller: true
+    },
+    
+]
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
+
+
+const Navbar = () => {
+  return (
+    <div className=' flex items-center justify-between py-5 font-medium'>
+
+      <img src={assets.logo} className='w-[36vh]'  alt="" />
+
+      <ul className=' hidden sm:flex gap-5 text-sm text-gray-600'>
+        
+      <NavLink to='/home' className=' flex flex-col items-center gap-1 '>
+            <p className='green hover:text-gray-950 duration-200'>HOME</p>
+            <hr className='green w-2/4 border-none h-[1.5px] bg-pink-500 hidden'/> 
+        </NavLink>
+          
+        <NavLink to='/collection' className=' flex flex-col items-center gap-1 '>
+            <p className='yellow hover:text-gray-950 duration-200'>COLLECTION</p>
+            <hr className='yellow w-2/4 border-none h-[1.5px] bg-pink-500 hidden'/> 
+        </NavLink>
+          
+        <NavLink to='/about' className=' flex flex-col items-center gap-1 '>
+            <p className='blue hover:text-gray-950 duration-200'>ABOUT</p>
+            <hr className='blue w-2/4 border-none h-[1.5px] bg-pink-500 hidden'/> 
+        </NavLink>
+          
+        <NavLink to='/contact' className=' flex flex-col items-center gap-1 '>
+            <p className='red hover:text-gray-950 duration-200'>CONTACT</p>
+            <hr className=' red  w-2/4 border-none h-[1.5px] bg-pink-500 hidden'/> 
+        </NavLink>
+          
+      </ul>
+      <div className=' flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-7 cursor-pointer' alt="" />
+        
+        <div className='group relative'>
+          <img className='w-6 cursor-pointer'  src={assets.profile_icon} alt=''/>
+          <div className=' group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className=' flex flex-col gap w-40 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className=' cursor-pointer hover:text-black'> My Profile</p>
+              <p className=' cursor-pointer hover:text-black'> Order</p>
+              <p className=' cursor-pointer hover:text-black'> Logout</p>
+            </div>
+          </div>
+        </div>
+        <Link to='/cart' className=' relative'>
+           <img src={assets.cart_icon} className=' w-9 min-w-5' alt=''/> 
+           <p className=' absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full '>10</p>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
